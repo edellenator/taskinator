b2c89e501a02589a99130fd2a3a404e5e0eda88e
Erik Dell <edellenator@gmail.com>
2022-03-20 13:34:01 -0700

    task type appears in appended list

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,14 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function (event){
     console.log(event);
     event.preventDefault();
-
+    var taskNameInput = document.querySelector ("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector ("select[name='task-type']").value;
     var listItemEl = document.createElement ("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "New task";
+    var taskInfoEl = document.createElement ("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
